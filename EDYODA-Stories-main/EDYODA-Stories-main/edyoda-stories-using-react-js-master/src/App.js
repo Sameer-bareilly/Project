a1249996c52dbfe088f82abc9b788d856ab3d5de
Sameer-bareilly <104997513+Sameer-bareilly@users.noreply.github.com>
2022-08-15 17:35:14 +0530

    Add files via upload

---
+++ EDYODA-Stories-main/EDYODA-Stories-main/edyoda-stories-using-react-js-master/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './Conponent/Header';
+import Post from './Conponent/Post';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Post />
+    </div>
+  );
+}
+
+export default App;
